920486b50b167c4b8800357cbef681a92de4c2b4
Ana Fotina <ana.fotina@mindcamp.se>
2020-05-03 11:07:42 +0200

    added load_img_data function for cn

---
+++ load_data.py
@@ -0,0 +1,22 @@
+import os
+from glob import glob
+import numpy as np
+from numpy import genfromtxt
+
+img_size = 536
+img_data = []
+img_label = []
+# Adjust your paths and provide a subfolder as a string, e.g. 'square_35_rotate'
+def load_img_data(subfolder, img_size = 536):
+    folder = os.getcwd() + '\\Data\\data\\' + subfolder
+    images = glob(os.path.join(folder, '*.png'))
+    labels = genfromtxt(folder + '\\output.txt', delimiter=',', skip_header=1)
+    for i in range(len(images)):
+        img = load_img(images[i], color_mode="grayscale", target_size=(img_size, img_size))
+        img = img_to_array(img)
+        lbl = labels[i]
+        img_data.append(img)
+        img_label.append(lbl)
+
+X = np.array(img_data)
+y = np.array(img_label)
