b43d3f7286371ef2b12f88bde932ac0fcac08131
Ana Fotina <ana.fotina@mindcamp.se>
2020-05-11 19:53:57 +0200

    Updated load data

--- Model.py
+++ Model.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import LearningRateScheduler
 
-r=1
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state = r)
+#r=1
+#X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state = r)
 
 # MODEL PARAMS
 batch_s = 16
--- load_data.py
+++ load_data.py
@@ -2,16 +2,17 @@ import os
 from glob import glob
 import numpy as np
 from numpy import genfromtxt
+import random
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 
-
+# LOAD IMAGES
 img_size = 536//2
 img_data = []
 img_label = []
 # Adjust your paths and provide a subfolder as a string, e.g. 'square_35_rotate'
 def load_img_data(subfolder, img_size = img_size):
-    folder = os.getcwd() + '\\Data\\data\\' + subfolder
+    folder = os.getcwd() + '\\data\\' + subfolder
     images = glob(os.path.join(folder, '*.png'))
     labels = genfromtxt(folder + '\\output.txt', delimiter=',', skip_header=1)
     for i in range(len(images)):
@@ -24,12 +25,37 @@ def load_img_data(subfolder, img_size = img_size):
         img_data.append(img)
         img_label.append(lbl)
 
-subfolders = ['square_35_rotate', 'square_35_rotate2', 'square_45_rotate', 'square_scale', 'solid']
+subfolders = ['square_35_rotate9000', 'square_scale1000']
 for s in subfolders:
     load_img_data(subfolder=s)
 
+# NORMALIZING
 X = np.array(img_data)/255
 y = np.array(img_label)
 print('X shape', X.shape)
 print('y shape', y.shape)
+
+# TRAIN, TEST, HOLDOUT SETS
+X_idx = list(range(X.shape[0]))
+random.seed(1)
+train_idx = random.sample(X_idx, int(X.shape[0]*0.8))
+random.seed(1)
+test_hold_idx = [x for x in X_idx if x not in train_idx]
+random.seed(1)
+test_idx = random.sample(test_hold_idx, len(test_hold_idx)//2)
+holdout_idx = [x for x in test_hold_idx if x not in test_idx]
+
+X_train = X[train_idx]
+y_train = y[train_idx]
+X_test = X[test_idx]
+y_test = y[test_idx]
+X_holdout = X[holdout_idx]
+y_holdout = y[holdout_idx]
+
+print('X train shape', X_train.shape)
+print('y train shape', y_train.shape)
+print('X test shape', X_test.shape)
+print('y test shape', y_test.shape)
+print('X holdout shape', X_holdout.shape)
+print('y holdout shape', y_holdout.shape)
 
